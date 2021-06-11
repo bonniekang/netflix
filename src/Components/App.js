70d205227f5ee3c46c464d45334a1fe3011548c0
jihye kang <jihye59kang@gmail.com>
2021-06-11 18:17:11 +0100

    reset dafault style and add header style

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Router from "./Router.js";
+import GlobalStyles from "./GlobalStyles.js"
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     )
   }
