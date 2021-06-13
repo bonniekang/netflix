275c3cce913bb655e49f2c1cfbcc60333fdb4371
jihye kang <jihye59kang@gmail.com>
2021-06-14 00:24:10 +0100

    home presenter children

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -9,7 +9,17 @@ const Container = styled.div`
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error}) => loading ? null : 
 <Container>
-    {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing" />}
+    {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => movie.title)}</Section>}
+    {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+    {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
 </Container>;
 
 HomePresenter.propTypes = {
