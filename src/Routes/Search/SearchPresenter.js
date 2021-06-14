e5e9c37af1ec4c6f540e421c11762e26fc796c1a
jihye kang <jihye59kang@gmail.com>
2021-06-14 12:16:02 +0100

    message component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components"
 import Loader from "../../Components/Loader"
 import Section from "../../Components/Section"
+import Message from "../../Components/Message"
 
 const Container = styled.div``;
 
@@ -22,6 +23,8 @@ const SearchPresenter = ({ movieResults, tvResults, searchTerm, loading, error, 
     {loading ? <Loader/> : <>
         {movieResults && movieResults.length > 0 && (<Section title="Movie Results">{movieResults.map(movie => (<span key={movie.id}>{movie.title}</span>))}</Section>)}
         {tvResults && tvResults.length > 0 && (<Section title="TV Results">{tvResults.map(show => (<span key={show.id}>{show.name}</span>))}</Section>)}
+        {error && <Message color="#e74c3c" text={error}/>}
+        {tvResults && movieResults && tvResults.length === 0 && movieResults.length === 0 && (<Message color="#95a5a6" text="Nothing found."/>)}
     </>}
 </Container>;
 
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import styled from "styled-components"
 import Section from "../../Components/Section"
 import Loader from "../../Components/Loader"
+import Message from "../../Components/Message"
 
 const Container = styled.div``
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error}) => loading ? <Loader /> : 
+const TVPresenter = ({ topRated, popular, airingToday, loading, error}) => loading ? <Loader /> : (
 <Container>
-    {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => show.name)}</Section>}
-    {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => show.name)}</Section>}
-    {airingToday && airingToday.length > 0 && <Section title="AiringToday Shows">{airingToday.map(show => show.name)}</Section>}
-</Container>;
+    {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => (<span key={show.id}>{show.name}</span>))}</Section>}
+    {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}</Section>}
+    {airingToday && airingToday.length > 0 && <Section title="AiringToday Shows">{airingToday.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}</Section>}
+    {error && <Message color="#e74c3c" text={error}/>}
+</Container>);
 
 TVPresenter.propTypes = {
     topRated: PropTypes.array,
