5344f14b818b59a05aa0d2e71563b1f643f76144
jihye kang <jihye59kang@gmail.com>
2021-06-15 20:55:00 +0100

    poster styling

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,29 +3,60 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 import {Link} from "react-router-dom"
 
-const Container = styled.div``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const ImageContainer = styled.div`
+    margin-bottom: 5px;
+    position: relative;
+    &:hover{
+        ${Image}{
+            opacity: 0.3;
+        }
+        ${Rating}{
+            opacity: 1;
+        }
+    }
+`;
 
-const Image = styled.div``;
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl})
+    background-size: cover;
+    height: 180px;
+    background-position: center center;
+    transition: opacity 0.2s linear;
+`;
 
-const Rating = styled.div``;
+const Rating = styled.div`
+    bottom: 5px;
+    right: 5px;
+    opacity: 0;
+    position: absolute;
+    transition: opacity 0.1s linear;
+`;
 
-const Title = styled.div``;
+const Title = styled.div`
+    display: block;
+    margin-bottom: 5px;
+`;
 
-const Year = styled.div``;
+const Year = styled.div`
+font-size: 10px;
+color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false}) => (
 <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
     <Container>
         <ImageContainer>
-            <Image bgUrl={imageUrl} />
+            <Image bgUrl={imageUrl ? `https://image.tmdb.org/t/p/w300${imageUrl}` : require("../assets/noPoster.png") } />
             <Rating>
                 <span role="img" aria-label="rating">star</span>{" "}{rating}/10
                 </Rating>
         </ImageContainer>
-        <Title>{title}</Title>
-        <Title>{year}</Title>
+        <Title>{title.length > 18 ? `${title.substring(0, 18)}...` : title}</Title>
+        <Year>{year}</Year>
     </Container>
  </Link>
 )
